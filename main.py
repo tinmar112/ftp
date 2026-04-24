a8670fe0366d08b3b318665571911c39af013e11
Martin Zadora <martin.zadora@etu.minesparis.psl.eu>
2026-04-23 22:52:42 -0400

    surface-reading

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from read_surface import SurfaceReader
+from surface_reader import SurfaceReader
 
 if __name__ == '__main__':
 
@@ -7,13 +7,14 @@ if __name__ == '__main__':
     path = './photos/30deg/more-fringes.jpg'
     step = 3.65e-5  # m/pixel
     L = 0.65        # m
-    D = 0.067       # m
+    D = 0.0675      # m
 
     surface_reader = SurfaceReader(path=path, path_ref=path_ref, step=step)
 
     # creating the expected profile
+    base, angle = 2.25e-2, np.pi/6
     expected_profile = np.zeros(shape=surface_reader._image_ref.shape)
-    half_triangle = np.linspace(0, 2.25*np.tan(np.pi/6),num=754) # 30-degree triangle
+    half_triangle = np.linspace(0, base*np.tan(angle),num=754) # 30-degree triangle
     triangle = np.append(half_triangle, half_triangle[::-1])
     expected_profile[1242:1242+754*2, :1491] = triangle[:, np.newaxis]
     
