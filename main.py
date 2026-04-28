a353c8b1af3512ce5cb736f87b96e98dac64e676
Martin Zadora <martin.zadora@etu.minesparis.psl.eu>
2026-04-28 13:42:38 -0400

    testing best-params

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from surface_reader import SurfaceReader
 
 if __name__ == '__main__':
 
-    path_ref = './photos/30deg/more-fringes-ref.jpg'
-    path = './photos/30deg/more-fringes.jpg'
+    path_ref = './photos/30deg/less-fringes-ref.jpg'
+    path = './photos/30deg/less-fringes.jpg'
     step = 3.65e-5  # m/pixel
     L = 0.65        # m
     D = 0.0675      # m
 
-    surface_reader = SurfaceReader(path=path, path_ref=path_ref, step=step)
-
     # creating the expected profile
-    base, angle = 2.25e-2, np.pi/6
-    expected_profile = np.zeros(shape=surface_reader._image_ref.shape)
-    half_triangle = np.linspace(0, base*np.tan(angle),num=754) # 30-degree triangle
-    triangle = np.append(half_triangle, half_triangle[::-1])
-    expected_profile[1242:1242+754*2, :1491] = triangle[:, np.newaxis]
-    
-    surface_reader.read(L=L, D=D, average=True)
+    end_x, start_y, width = 1491, 1340, 1508
+    base, angle = 5.5e-2, np.pi/6
 
-    surface_reader.compare_with(expected_profile=expected_profile)
+    expected_profile = np.zeros(shape=(4096,2048))
+    half_triangle = np.linspace(0, (base/2)*np.tan(angle),num=width//2) # 30-degree triangle
+    triangle = np.append(half_triangle, half_triangle[::-1])
+    expected_profile[start_y: start_y+width, :end_x] = triangle[:, np.newaxis]
+
+    # read profile + compare to reality
+    surface_reader = SurfaceReader(path=path, path_ref=path_ref, step=step)
+    surface_reader.read(filter_width=0.36, L=L, D=D, average=False)
+    error = surface_reader.error(expected_profile=expected_profile, show='2D')
--- surface_reader.py
+++ surface_reader.py
@@ -20,44 +20,57 @@ class SurfaceReader:
         self._image_ref: np.ndarray = np.array(jpg0)
         self._image: np.ndarray = np.array(jpg)
 
-    def read(self, L: float, D: float, average: bool) -> None:
+    def read(self, filter_width: float,
+             L: float, D: float, average: bool) -> None:
         """Reads the surface profile by FTP and displays it."""
 
         ftp = FTP(image=self._image, image_ref=self._image_ref,
                   step_x=self._step, step_y=self._step,
-                  window_beta=3, padding=0.1, filter_width=0.634)
+                  window_beta=3, padding=0.1,
+                  filter_width=filter_width)
         
         ftp.compute(plot_spectrum=False)
         delta_phi = ftp.phase_diff()
 
-        profile = height(delta_phi=delta_phi,
-                         p=ftp.p, L=L, D=D)
-        
-        self._profile = profile
+        self._profile = height(delta_phi=delta_phi, p=ftp.p, L=L, D=D)
 
         if average:
-            self._profile = uniform_filter(self._profile, size=5, mode='constant')
+            self._profile = uniform_filter(self._profile, size=3, mode='constant', axes=0)
+            self._profile = uniform_filter(self._profile, size=5, mode='constant', axes=1)
         
         # displaying
-        extent = (0, profile.shape[1] * self._step, 0, profile.shape[0] * self._step)
-        plt.imshow(profile, cmap='jet', extent=extent)
+        profile = self._profile
+        extent = (0, profile.shape[1] * self._step / 1e-3, 0, profile.shape[0] * self._step / 1e-3) # type: ignore
+        plt.imshow(profile/1e-3, cmap='jet', extent=extent)
         plt.title('Height profile')
-        plt.xlabel(r'$x \: (m)$')
-        plt.ylabel(r'$y \: (m)$')
-        plt.colorbar(label='Height (m)')
+        plt.xlabel(r'$x \: (mm)$')
+        plt.ylabel(r'$y \: (mm)$')
+        plt.colorbar(label='Height (mm)')
         plt.show()
     
-    def compare_with(self, expected_profile: np.ndarray) -> None:
+    def error(self, expected_profile: np.ndarray, show: str ) -> float:
 
         error = self._profile - expected_profile
-        
-        print(error.sum().sum())
 
-        # displaying
-        extent = (0, error.shape[1] * self._step, 0, error.shape[0] * self._step)
-        plt.imshow(error/1e-3, cmap='gray', extent=extent)
-        plt.title('Height error = Reconstructed - Expected')
-        plt.xlabel(r'$x \: (m)$')
-        plt.ylabel(r'$y \: (m)$')
-        plt.colorbar(label='Error (mm)')
-        plt.show()
+        if show == '2D':
+            # Cross-section
+            y = np.linspace(0, error.shape[0] * self._step, num=len(error))/1e-3
+            plt.plot(y, self._profile[:,1312]/1e-3, label='Reconstruction')
+            plt.plot(y, expected_profile[:,1312]/1e-3, label='Expected')
+            plt.xlabel('y (mm)')
+            plt.ylabel('Height (mm)')
+            plt.title('Cross-section')
+            plt.legend()
+            plt.show()
+
+        elif show == '3D':
+            # Heat-map
+            extent = (0, error.shape[1] * self._step, 0, error.shape[0] * self._step)
+            plt.imshow(error/1e-3, cmap='gray', extent=extent)
+            plt.title('Height error = Reconstructed - Expected')
+            plt.xlabel(r'$x \: (m)$')
+            plt.ylabel(r'$y \: (m)$')
+            plt.colorbar(label='Error (mm)')
+            plt.show()
+
+        return (error ** 2).sum()
